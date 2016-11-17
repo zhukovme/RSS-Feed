ba7da7e50b31742862878c0eda0ba292b1ef49b9
mzhukov <mzhukov@ccsteam.ru>
2016-11-17 23:34:19 +0300

    Setup widget project

---
+++ app/src/main/java/zhukovme/rsswidget/RssWidget.java
@@ -0,0 +1,12 @@
+package zhukovme.rsswidget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class RssWidget extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+    }
+}
